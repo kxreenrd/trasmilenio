1f062d2ed1bfcd0fae0aee40cfe56d1f49de8038
Karen Rodríguez <krodrigue51@uniminuto.edu.co>
2019-10-03 21:25:48 -0500

    FINAL

--- trasmilenio/js/generador.js
+++ trasmilenio/js/generador.js
@@ -1,180 +1,187 @@
 var arr_mixto = [];
-var a = Math.floor(Math.random() * 10) + 1
-var c = Math.floor(Math.random() * 10) + 1
+var a = Math.floor(Math.random() * 10) + 1;
+var c = Math.floor(Math.random() * 10) + 1;
 var m = 7;
-var x1 = Math.floor(Math.random() * 10) + 1
-var n = 10;
+var x1 = Math.floor(Math.random() * 10) + 1;
+var n = 10000;
 var p = 0;
 var opcion = 1;
 
 
 class generador {
-  generar(op) {
-    opcion = op;
-    console.log('2-> a ', a, 'c ', c, 'm ', m, 'semilla ', x1, 'opcion ', opcion);
+    generar(op) {
+        opcion = op;
+        console.log('2-> a ', a, 'c ', c, 'm ', m, 'semilla ', x1, 'opcion ', opcion);
 
-    for (var i = 0; i < n; i++) {
-      if (op == 1) { //mixto
+        for (var i = 0; i < n; i++) {
+            if (op == 1) { //mixto
 
-        var div = x1 / m;
-        g.repetidos(arr_mixto, div);
-        arr_mixto.push(div);
-        x1 = g.mixto(x1, a, c, m);
+                var div = x1 / m;
+                g.repetidos(arr_mixto, div);
+                arr_mixto.push(div);
+                x1 = g.mixto(x1, a, c, m);
 
-      } else if (op == 2) { //multi
-        arr_mixto.push(x1);
-        x1 = multi(x1, a, m);
-      }
-    }
-    g.prueba_promedio();
-
-  }
-
-  mixto(semilla, a, c, m) {
-    //x1 = (aX + c) mod m
-    //console.log('a',a,'c', c, 'm', m, 'x1', x1, 'semilla', semilla);
-    var mul = a * semilla;
-    var sum = mul + c;
-    var x1 = sum % m;
-    //console.log('r mixto', x1, (x1/m));
-    return x1;
-
-  }
-
-  multi(semilla, a, m) {
-    //x1 = (aX ) mod m
-    var multi = a * semilla;
-    var x1 = multi % m;
-    return x1;
-  }
-
-  repetidos(arr, n) {
-    //console.log('arr',arr.length);
-    for (var i in arr) {
-      if (arr[i] == n) {
-        //console.log('es repetido', n, arr[i], i, m);
-        m = m * 3;
-        a = Math.floor(Math.random() * 10) + 1
-        c = Math.floor(Math.random() * 10) + 1
-        //var x1 = Math.random(10);
-      }
-    }
-  }
-
-
-  prueba_promedio() {
-    console.log('arr_mixto ', arr_mixto);
-    var sum = 0;
-    for (var i in arr_mixto) {
-      sum += arr_mixto[i];
-    }
-    var promedio = sum / n;
-    var raiz = Math.sqrt(n);
-    var denom = Math.sqrt((1 / 12));
-
-    var numerador = (promedio - (1 / 2)) * raiz;
-
-    var resul = numerador / denom;
-    resul = Math.abs(resul);
-    //console.log('resul',resul);
-    if (resul < 1.96) {
-      console.log('si que si');
-      //tiempo();
-      g.prueba_frecuencia();
-    } else {
-      console.log('no que no');
-      arr_mixto = [];
-      g.generar(opcion);
-    }
-
-
-  }
-
-  prueba_frecuencia() {
-    var pq1 = 0,
-      pq2 = 0,
-      pq3 = 0,
-      pq4 = 0,
-      pq5 = 0;
-    for (var i in arr_mixto) {
-      if (arr_mixto[i] >= 0 && arr_mixto[i] < 0.2) {
-        pq1++;
-      } else if (arr_mixto[i] >= 0.2 && arr_mixto[i] < 0.4) {
-        pq2++;
-      } else if (arr_mixto[i] >= 0.4 && arr_mixto[i] < 0.6) {
-        pq3++;
-      } else if (arr_mixto[i] >= 0.6 && arr_mixto[i] < 0.8) {
-        pq4++;
-      } else if (arr_mixto[i] >= 0.8 && arr_mixto[i] <= 1) {
-        pq5++;
-      } else {
-        console.log('no esta en ningun rango, ', arr_mixto[i]);
-      }
-    }
-    var fe = n / 5;
-    for (var i = 0; i < 5; i++) {
+            } else if (op == 2) { //multi
+                arr_mixto.push(x1);
+                x1 = multi(x1, a, m);
+            }
+        }
+        g.prueba_promedio();
 
     }
-    var resta = pq1 - fe;
-    var den = Math.pow(resta, 2);
-    var res = den / fe;
-    console.log('paquete 1:', pq1);
-    console.log('paquete 2:', pq2);
-    console.log('paquete 3:', pq3);
-    console.log('paquete 4:', pq4);
-    console.log('paquete 5:', pq5);
-    console.log('fe:', fe, 'res: ', res, 'x2: ', Math.pow(res, 2));
-    document.getElementById('aprobar').style.display = 'block';
+
+    mixto(semilla, a, c, m) {
+        //x1 = (aX + c) mod m
+        //console.log('a',a,'c', c, 'm', m, 'x1', x1, 'semilla', semilla);
+        var mul = a * semilla;
+        var sum = mul + c;
+        var x1 = sum % m;
+        //console.log('r mixto', x1, (x1/m));
+        return x1;
+
+    }
+
+    multi(semilla, a, m) {
+        //x1 = (aX ) mod m
+        var multi = a * semilla;
+        var x1 = multi % m;
+        return x1;
+    }
+
+    repetidos(arr, n) {
+        //console.log('arr',arr.length);
+        for (var i in arr) {
+            if (arr[i] == n) {
+                //console.log('es repetido', n, arr[i], i, m);
+                m = m * 3;
+                a = Math.floor(Math.random() * 10) + 1
+                c = Math.floor(Math.random() * 10) + 1
+                //var x1 = Math.random(10);
+            }
+        }
+    }
+
+    prueba_promedio() {
+        var resul = 0, promedio = 0, raiz =0, denom =0,numerador= 0;
+        console.log('arr_mixto ', arr_mixto);
+        var sum = 0;
+        for (var i in arr_mixto) {
+            sum += arr_mixto[i];
+        }
+        promedio = sum / n;
+        raiz = Math.sqrt(n);
+        denom = Math.sqrt((1 / 12));
+
+        numerador = (promedio - (1 / 2)) * raiz;
+
+        resul = numerador / denom;
+        resul = Math.abs(resul);
+        console.log('resul',resul);
+        document.getElementById('promedio').innerHTML = resul;
+        if (resul < 1.96) {
+            console.log('si que si');
+            //tiempo();
+            g.prueba_frecuencia();
+        } else {
+            console.log('no que no');
+            document.getElementById('aprobar').style.display = 'none';
+            arr_mixto = [];
+            g.generar(opcion);
+        }
+
+
+    }
+
+    prueba_frecuencia() {
+        var pq1 = 0,
+                pq2 = 0,
+                pq3 = 0,
+                pq4 = 0,
+                pq5 = 0;
+        for (var i in arr_mixto) {
+            if (arr_mixto[i] >= 0 && arr_mixto[i] < 0.2) {
+                pq1++;
+            } else if (arr_mixto[i] >= 0.2 && arr_mixto[i] < 0.4) {
+                pq2++;
+            } else if (arr_mixto[i] >= 0.4 && arr_mixto[i] < 0.6) {
+                pq3++;
+            } else if (arr_mixto[i] >= 0.6 && arr_mixto[i] < 0.8) {
+                pq4++;
+            } else if (arr_mixto[i] >= 0.8 && arr_mixto[i] <= 1) {
+                pq5++;
+            } else {
+                console.log('no esta en ningun rango, ', arr_mixto[i]);
+            }
+        }
+        var fe = n / 5;
+        var sumatoria = g.suma(pq1, fe) + g.suma(pq2, fe) + g.suma(pq3, fe) + g.suma(pq4, fe) + g.suma(pq5, fe);
+        /*var resta = pq1 - fe;
+        var den = Math.pow(resta, 2);
+        var res = den / fe;*/
+        console.log('paquete 1:', pq1);
+        console.log('paquete 2:', pq2);
+        console.log('paquete 3:', pq3);
+        console.log('paquete 4:', pq4);
+        console.log('paquete 5:', pq5);
+        console.log('fe:', fe, 'sumatoria: ', sumatoria, 'x2: ', Math.pow(sumatoria, 2));
+        document.getElementById('frecuencia').innerHTML = 'x: '+ sumatoria;
+        document.getElementById('frecuencia1').innerHTML = 'x²: '+ Math.pow(sumatoria, 2);
+        document.getElementById('aprobar').style.display = 'block';
+
+
+    }
     
-
-  }
-
-  prueba() {
-    for (var i = 0; i < 100; i++) {
-      console.log(i, '->', g.pedir_dato());
+    suma(pq, fe){
+        var resta = pq - fe;
+        var den = Math.pow(resta, 2);
+        var res = den / fe;
+        console.log(res);
+        return res;
     }
-  }
 
-
-  pedir_dato() {
-    console.log('pos: ', p);
-    p++;
-    return arr_mixto[p];
-
-  }
-
-
-  vista(id) {
-    opcion = id;
-    if (id == 1) {
-      document.getElementById('g_c').style.display = 'block';
-    } else {
-      document.getElementById('g_c').style.display = 'none';
+    prueba() {
+        for (var i = 0; i < 100; i++) {
+            console.log(i, '->', g.pedir_dato());
+        }
     }
-  }
 
-  definir() {
-    a = document.getElementById('a').value;
-    c = document.getElementById('c').value;
-    m = document.getElementById('m').value;
-    x1 = document.getElementById('semilla').value;
-    if (!a || !m || !x1) {
-      console.log('vaciooooo');
-      document.getElementById('alert').style.display = 'block';
-    } else {
-      document.getElementById('alert').style.display = 'none';
-      console.log('a ', a, 'c ', c, 'm ', m, 'semilla ', x1, 'opcion ', opcion);
-      //g.arr();
-      g.generar(opcion);
-
+    pedir_dato() {
+        console.log('pos: ', p);
+        p++;
+        return arr_mixto[p];
 
     }
 
-  }
+    vista(id) {
+        opcion = id;
+        if (id == 1) {
+            document.getElementById('g_c').style.display = 'block';
+        } else {
+            document.getElementById('g_c').style.display = 'none';
+        }
+    }
 
-  arr(){
-    console.log(arr_mixto);
-    return arr_mixto;
-  }
+    definir() {
+        document.getElementById('resultados_p').style.display = 'block';
+        a = document.getElementById('a').value;
+        c = document.getElementById('c').value;
+        m = document.getElementById('m').value;
+        x1 = document.getElementById('semilla').value;
+        if (!a || !m || !x1) {
+            console.log('vaciooooo');
+            document.getElementById('alert').style.display = 'block';
+        } else {
+            document.getElementById('alert').style.display = 'none';
+            console.log('1-> a ', a, 'c ', c, 'm ', m, 'semilla ', x1, 'opcion ', opcion);
+            //g.arr();
+            g.generar(opcion);
+        }
+
+    }
+
+    arr() {
+        console.log('entro a arr', arr_mixto);
+        return arr_mixto;
+    }
 }
 const g = new generador();
--- trasmilenio/js/operaciones.js
+++ trasmilenio/js/operaciones.js
@@ -25,229 +25,272 @@ var bus_d = 0;
 var buses_derecha = 0;
 var buses_izquierda = 0;
 
+var buses_derecha_l = 0;
+var buses_izquierda_l = 0;
+
 var t_buses = {};
 
 function tiempo() {
-  
-  console.log(g.arr());
-     //d = data.split(',');
-     //d = data;//.split(',');
-     var n = 0;
-     var l = document.getElementById("contador");
-     var usu_ac = document.getElementById("usuarios_actuales");
-     var im = window.setInterval(function() {
-          hora(n);
-          if (n < time_total) {
-               l.innerHTML = n; //tiempo
-               //console.log('>',d[n]);
+    document.getElementById('gen').style.display = 'none';
+    //console.log(g.arr());
+    //d = data.split(',');
+    //d = data;//.split(',');
+    var n = 0;
+    var l = document.getElementById("contador");
+    var usu_ac = document.getElementById("usuarios_actuales");
+    var im = window.setInterval(function () {
+        hora(n);
+        if (n < time_total) {
+            l.innerHTML = n; //tiempo
+            //console.log('>',d[n]);
 
-               var ent = g.pedir_dato();
-               //var ent = d[n] * 10;
-               var round = enteros(ent);
-               usu_actu += round;
-               //console.log('usu_actu', usu_actu);
-               if (usu_actu < capacidad_estacion) {
-                    ingreso_estacion(usu_actu, g.pedir_dato(), round);
-                    document.getElementById('c_estacion').innerHTML = usu_actu;
-                    usu_ac.innerHTML = usu_actu; //usuarios actuales
-               } else {
-                    clearInterval(im);
-                    ingreso_estacion(-1);
-                    console.log('ACABO', usu_actu, u);
-               }
-               n++;
-          } else {
-               clearInterval(im);
-          }
-          /*if (tiempo_suma >= time_total) {
-              clearInterval(im);
-          }
-          tiempo_suma = tiempo_suma + time_interval;*/
+            var ent = g.pedir_dato();
+            //var ent = d[n] * 10;
+            var round = enteros(ent);
+            usu_actu += round;
+            //console.log('usu_actu', usu_actu);
+            if (usu_actu < capacidad_estacion) {
+                ingreso_estacion(usu_actu, g.pedir_dato(), round);
+                document.getElementById('c_estacion').innerHTML = usu_actu;
+                usu_ac.innerHTML = usu_actu; //usuarios actuales
+            } else {
+                clearInterval(im);
+                ingreso_estacion(-1);
+                console.log('ACABO', usu_actu, u);
+            }
+            n++;
+        } else {
+            clearInterval(im);
+        }
+        /*if (tiempo_suma >= time_total) {
+         clearInterval(im);
+         }
+         tiempo_suma = tiempo_suma + time_interval;*/
 
-     }, time_interval);
+    }, time_interval);
 
 }
 
 function hora(n) {
-     var div = document.getElementById('tiempo');
-     //console.log(n);
+    var div = document.getElementById('tiempo');
+    //console.log(n);
 
-     var horas = 4,
-          minutos = Math.round(n);
-     if (minutos % 5 == 0 && minutos > 4) {
-          console.log(minutos, 'minuto chao', chai);
-          t_buses[minutos + '_ida'] = {
-               'bus_a': bus_a,
-               'bus_b': bus_b,
-               'bus_c': bus_c,
-               'bus_d': bus_d
-          };
-          console.log(t_buses);
-          buses(n, minutos);
-     }
-     if (minutos >= 60) {
-          var min = Math.trunc(minutos / 60);
-          horas += min;
-          minutos -= (min * 60);
+    var horas = 4,
+            minutos = Math.round(n);
+    if (minutos % 5 == 0 && minutos > 4) {
+        console.log(minutos, 'minuto chao', chai);
+        t_buses[minutos + '_ida'] = {
+            'bus_a': bus_a,
+            'bus_b': bus_b,
+            'bus_c': bus_c,
+            'bus_d': bus_d
+        };
+        console.log(t_buses);
+        buses(n, minutos);
+    }
+    if(minutos % 30 == 0 ){
+        limpiar_buses();
+    }
+    if (minutos >= 60) {
+        var min = Math.trunc(minutos / 60);
+        horas += min;
+        minutos -= (min * 60);
 
-     }
-     var v_m = (minutos < 10) ? '0' + minutos : minutos;
-     div.innerHTML = horas + ':' + v_m;
-     document.getElementById('time1').innerHTML = horas + ':' + v_m;
-     //console.log('HORA:', horas, ':', v_m);
+    }
+    var v_m = (minutos < 10) ? '0' + minutos : minutos;
+    div.innerHTML = horas + ':' + v_m;
+    document.getElementById('time1').innerHTML = horas + ':' + v_m;
+    //console.log('HORA:', horas, ':', v_m);
 }
 /*
  Devuelve el numero aleatorio decimal en entero
  */
 function enteros(ent) {
 
-     var round = 0;
-     if (ent >= 1) {
-          round = Math.round(ent);
-     } else {
-          ent *= 10;
-          round = Math.round(ent);
-     }
-     //console.log('entero: ',round);
-     return round;
+    var round = 0;
+    if (ent >= 1) {
+        round = Math.round(ent);
+    } else {
+        ent *= 10;
+        round = Math.round(ent);
+    }
+    //console.log('entero: ',round);
+    return round;
 }
 /*
  Valida el ingreso a la estación
  */
 function ingreso_estacion(ingreso, n = null, cant = null) {
-     var div = document.getElementById('estacion');
+    var div = document.getElementById('estacion');
 
-     if (ingreso < 0) {
-          div.style.border = '2px solid #ec7c0d';
-          div.classList.add('animated', 'flash');
-          //console.log('aqui que', usu_actu);
-     } else {
-          //u++;
+    if (ingreso < 0) {
+        div.style.border = '2px solid #ec7c0d';
+        div.classList.add('animated', 'flash');
+        //console.log('aqui que', usu_actu);
+    } else {
+        //u++;
 
-          if (cant > 0) {
-               var inter = time_interval / cant;
-               var i = 1;
-               var it = window.setInterval(function() {
-                    u++;
-                    var node = document.createElement("div");
-                    node.id = 'p_' + u;
-                    //console.log(,u)
-                    //node.innerHTML = u;
-                    node.className = tipo_persona(n) + ' animated bounceInLeft';
-                    document.getElementById("estacion").appendChild(node);
-                    if (i == cant) {
-                         clearInterval(it);
-                    }
-                    a_que_bus(n, node, u, i);
-                    i++
-                    //clearInterval(it);
-               }, inter);
-               //console.log('it',it);
-          }
-          //u++;
-     }
+        if (cant > 0) {
+            var inter = time_interval / cant;
+            var i = 1;
+            var it = window.setInterval(function () {
+                u++;
+                var node = document.createElement("div");
+                node.id = 'p_' + u;
+                //console.log(,u)
+                //node.innerHTML = u;
+                node.className = tipo_persona(g.pedir_dato()) + ' animated bounceInLeft';
+                document.getElementById("estacion").appendChild(node);
+                if (i == cant) {
+                    clearInterval(it);
+                }
+                a_que_bus(n, node, u, i);
+                i++
+                //clearInterval(it);
+            }, inter);
+            //console.log('it',it);
+        }
+        //u++;
+}
 }
 
 
 /*
-
+ 
  */
 function tipo_persona(rnd) {
-     var rt;
-     if (rnd > 0.6) { //adultos
-          rt = 'rombo';
-     } else if (rnd < 0.05) { //discapacitadas
-          rt = 'cuadrado ';
-     } else { //niños
-          rt = 'circulo';
-     }
-     return rt;
+    var rt;
+    if (rnd > 0.6) { //adultos
+        rt = 'rombo';
+    } else if (rnd < 0.05) { //discapacitadas
+        rt = 'cuadrado ';
+    } else { //niños
+        rt = 'circulo';
+    }
+    return rt;
 }
 
 
 function a_que_bus(rnd, node, u, i) {
-     if (rnd > 0 && rnd <= 0.25) {
-          if (bus_a < capacidad_bus) {
-               style_person('#007bff', 'bounceInUp', 'busa');
-               document.getElementById('c_ba').innerHTML = bus_a + ' A';
-               bus_a++;
-               buses_derecha++;
-          }
-     } else if (rnd > 0.25 && rnd <= 0.5) {
-          if (bus_b < capacidad_bus) {
-               style_person('#17a2b8', 'bounceInUp', 'busb');
-               document.getElementById('c_bb').innerHTML = bus_b + ' B';
-               bus_b++;
-               buses_derecha++;
-          }
-     } else if (rnd > 0.5 && rnd <= 0.75) {
-          if (bus_c < capacidad_bus) {
-               style_person('#004085', 'bounceInDown', 'busc');
-               document.getElementById('c_bc').innerHTML = bus_c + ' C';
-               bus_c++;
-               buses_izquierda++;
-          }
-     } else if (rnd > 0.75 && rnd <= 1) {
-          if (bus_d < capacidad_bus) {
-               style_person('#383d41', 'bounceInDown', 'busd');
-               document.getElementById('c_bd').innerHTML = bus_d + ' D';
-               bus_d++;
-               buses_izquierda++;
-          }
-     }
+    if (rnd > 0 && rnd <= 0.25) {
+        if (bus_a < capacidad_bus) {
+            style_person('#007bff', 'bounceInUp', 'busa');
+            document.getElementById('c_ba').innerHTML = bus_a + ' A';
+            bus_a++;
+            buses_derecha++;
+        }
+    } else if (rnd > 0.25 && rnd <= 0.5) {
+        if (bus_b < capacidad_bus) {
+            style_person('#17a2b8', 'bounceInUp', 'busb');
+            document.getElementById('c_bb').innerHTML = bus_b + ' B';
+            bus_b++;
+            buses_derecha++;
+        }
+    } else if (rnd > 0.5 && rnd <= 0.75) {
+        if (bus_c < capacidad_bus) {
+            style_person('#004085', 'bounceInDown', 'busc');
+            document.getElementById('c_bc').innerHTML = bus_c + ' C';
+            bus_c++;
+            buses_izquierda++;
+        }
+    } else if (rnd > 0.75 && rnd <= 1) {
+        if (bus_d < capacidad_bus) {
+            style_person('#383d41', 'bounceInDown', 'busd');
+            document.getElementById('c_bd').innerHTML = bus_d + ' D';
+            bus_d++;
+            buses_izquierda++;
+        }
+    }
 }
 
 function style_person(color, movimiento, bus) {
-     //console.log('u->',u);
-     if (u > 1) {
-          var at = document.getElementById('p_' + (u - 1));
-          var cll = (at.getAttribute('class')).split(' ');
-          //at.style.background = color;
-          at.className = cll[0] + ' animated ' + movimiento;
-          $(at).detach().appendTo('#' + bus);
-          //console.log('usu_actu1',usu_actu);
-          usu_actu--;
-          //console.log('usu_actu2',usu_actu);
-          //console.log(' resta usu_actu', usu_actu);
-     }
+    //console.log('u->',u);
+    if (u > 1) {
+        var at = document.getElementById('p_' + (u - 1));
+        var cll = (at.getAttribute('class')).split(' ');
+        //at.style.background = color;
+        at.className = cll[0] + ' animated ' + movimiento;
+        $(at).detach().appendTo('#' + bus);
+        //console.log('usu_actu1',usu_actu);
+        usu_actu--;
+        //console.log('usu_actu2',usu_actu);
+        //console.log(' resta usu_actu', usu_actu);
+    }
 
 }
 var chai = 0;
 
 function buses(n, minutos) {
-     chai++;
-     document.getElementById('busa').classList.add('animated', 'bounceInLeft');
-     document.getElementById('busb').classList.add('animated', 'bounceInLeft');
-     document.getElementById('busc').classList.add('animated', 'bounceInRight');
-     document.getElementById('busd').classList.add('animated', 'bounceInRight');
-     setTimeout(function() {
-          document.getElementById('busa').classList.remove('animated', 'bounceInLeft');
-          //document.getElementById('busa').innerHTML = '';
-          document.getElementById('busb').classList.remove('animated', 'bounceInLeft');
-          //document.getElementById('busb').innerHTML = '';
-          document.getElementById('busc').classList.remove('animated', 'bounceInRight');
-          //document.getElementById('busc').innerHTML = '';
-          document.getElementById('busd').classList.remove('animated', 'bounceInRight');
-          //document.getElementById('busd').innerHTML = '';
-     }, 900);
-     //console.log('- ', pedir_dato(), n, u);
+    chai++;
+    document.getElementById('busa').classList.add('animated', 'bounceInLeft');
+    document.getElementById('busb').classList.add('animated', 'bounceInLeft');
+    document.getElementById('busc').classList.add('animated', 'bounceInRight');
+    document.getElementById('busd').classList.add('animated', 'bounceInRight');
+    setTimeout(function () {
+        document.getElementById('busa').classList.remove('animated', 'bounceInLeft');
+        //document.getElementById('busa').innerHTML = '';
+        document.getElementById('busb').classList.remove('animated', 'bounceInLeft');
+        //document.getElementById('busb').innerHTML = '';
+        document.getElementById('busc').classList.remove('animated', 'bounceInRight');
+        //document.getElementById('busc').innerHTML = '';
+        document.getElementById('busd').classList.remove('animated', 'bounceInRight');
+        //document.getElementById('busd').innerHTML = '';
+    }, 900);
+    //console.log('- ', pedir_dato(), n, u);
+    
+    llegada(minutos);
+    
+    
+}
 
-     /*bus_a = enteros(pedir_dato());
-     bus_b = enteros(pedir_dato());
-     bus_c = enteros(pedir_dato());
-     bus_d = enteros(pedir_dato());*/
-     //console.log('bus_a', bus_a, 'bus_b', bus_b, 'bus_c',bus_c, 'bus_d', bus_d);
-     t_buses[minutos + '_llegada'] = {
-          'bus_a': bus_a,
-          'bus_b': bus_b,
-          'bus_c': bus_c,
-          'bus_d': bus_d
-     };
-     respuestas();
+function llegada(minutos){
+    bus_a = enteros(g.pedir_dato());
+    bus_b = enteros(g.pedir_dato());
+    bus_c = enteros(g.pedir_dato());
+    bus_d = enteros(g.pedir_dato());
+    
+    //console.log('bus_a', bus_a, 'bus_b', bus_b, 'bus_c',bus_c, 'bus_d', bus_d);
+    t_buses[minutos + '_llegada'] = {
+        'bus_a': bus_a,
+        'bus_b': bus_b,
+        'bus_c': bus_c,
+        'bus_d': bus_d
+    };
+    buses_derecha_l = bus_a+bus_b;
+    buses_izquierda_l = bus_c+bus_d;
+    
+    respuestas();
 }
 
 function respuestas() {
-     document.getElementById('q_1').innerHTML = buses_derecha + buses_izquierda;
-     document.getElementById('q_2').innerHTML = buses_derecha;
-     document.getElementById('q_3').innerHTML = buses_izquierda;
+    
+    for (var i = 0; i < 10000; i++) {
+        console.log()
+    }
+    
+    document.getElementById('q_1').innerHTML = buses_derecha + buses_izquierda;
+    document.getElementById('q_2').innerHTML = buses_derecha;
+    document.getElementById('q_3').innerHTML = buses_izquierda;
+    document.getElementById('q_4').innerHTML = buses_derecha_l + buses_izquierda_l;
+    document.getElementById('q_5').innerHTML = buses_derecha_l;
+    document.getElementById('q_6').innerHTML = buses_izquierda_l;
+    
+    
+    
+    //setTimeout(function () {document.getElementById('busa').innerHTML = '';}, 50);
+}
+
+function limpiar_buses(){
+    if(bus_a < 10){
+        document.getElementById('busa').innerHTML = '';
+    }
+    if(bus_b < 10){
+        document.getElementById('busb').innerHTML = '';
+    }
+    if(bus_c < 10){
+        document.getElementById('busc').innerHTML = '';
+    }
+    if(bus_d < 10){
+        document.getElementById('busd').innerHTML = '';
+    }
 }
